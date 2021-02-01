730b7a8b173e8568a6e46f74f4f94bc14e90501b
Francisco Martínez Lasaca <frml@itu.dk>
2021-02-01 03:30:53 +0100

    Simplify field parser

---
+++ type_splitter/tests/test.py
@@ -0,0 +1,79 @@
+import unittest
+import os
+from topic2type.main import line2field
+
+
+class TestTopic2Type(unittest.TestCase):
+    @staticmethod
+    def relative_file(path):
+        return os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
+
+    def test_positive_tests(self):
+        with open(self.relative_file("positive_tests.txt")) as f:
+            positive_tests = f.readlines()
+        for field in positive_tests:
+            with self.subTest(field=field):
+                self.assertIsNotNone(line2field(field))
+
+    def test_negative_tests(self):
+        with open(self.relative_file("negative_tests.txt")) as f:
+            negative_tests = f.readlines()
+        for field in negative_tests:
+            with self.subTest(field=field):
+                self.assertIsNone(line2field(field))
+
+    def test_positive_primitive_type(self):
+        field = "string my_string"
+        processed = line2field(field)
+        self.assertEqual(processed.type.type_name, "string")
+        self.assertEqual(processed.name, "my_string")
+        self.assertFalse(processed.is_array)
+
+    def test_positive_composite_type(self):
+        field = "another_pkg/YetAnotherMessage val"
+        processed = line2field(field)
+        self.assertEqual(processed.type.type_name, "another_pkg/YetAnotherMessage")
+        self.assertEqual(processed.name, "val")
+        self.assertFalse(processed.is_array)
+
+    def test_default_with_equal(self):
+        field = "int64 X=[25, 10, 3]"
+        processed = line2field(field)
+        self.assertEqual(processed.type.type_name, "int64")
+        self.assertEqual(processed.name, "X")
+        self.assertEqual(processed.default, "[25, 10, 3]")
+        self.assertFalse(processed.is_array)
+
+    def test_default_with_space(self):
+        field = "int64 X [25, 10, 3]"
+        processed = line2field(field)
+        self.assertEqual(processed.type.type_name, "int64")
+        self.assertEqual(processed.name, "X")
+        self.assertEqual(processed.default, "[25, 10, 3]")
+        self.assertFalse(processed.is_array)
+
+    def test_bounded_string(self):
+        field = "string<=15 length"
+        processed = line2field(field)
+        self.assertEqual(processed.type.type_name, "string")
+        self.assertEqual(processed.name, "length")
+        self.assertEqual(processed.string_max_size, 15)
+        self.assertFalse(processed.is_array)
+
+    def test_fixed_array_length(self):
+        field = "int64[5] values"
+        processed = line2field(field)
+        self.assertEqual(processed.type.type_name, "int64")
+        self.assertEqual(processed.name, "values")
+        self.assertEqual(processed.array_size, 5)
+        self.assertTrue(processed.is_array)
+        self.assertFalse(processed.is_bounded_array_size)
+
+    def test_bounded_array_length(self):
+        field = "int64[<=5] values"
+        processed = line2field(field)
+        self.assertEqual(processed.type.type_name, "int64")
+        self.assertEqual(processed.name, "values")
+        self.assertEqual(processed.array_size, 5)
+        self.assertTrue(processed.is_array)
+        self.assertTrue(processed.is_bounded_array_size)
