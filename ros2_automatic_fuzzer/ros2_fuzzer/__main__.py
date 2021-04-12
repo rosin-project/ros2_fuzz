ba66cfbfdbfd1807a6271e4194e6f373f6957f81
Francisco Martínez Lasaca <frml@itu.dk>
2021-04-12 02:10:57 +0200

    Refactor service fuzzer

--- ros2_automatic_fuzzer/ros2_fuzzer/__main__.py
+++ ros2_automatic_fuzzer/ros2_fuzzer/__main__.py
@@ -1,9 +1,9 @@
 import argparse
 import logging
 import os
-import json
 import logging
-import yamale
+from .service_fuzzer.__main__ import generate_template
+from .yaml_verification import read_and_validate_yaml_file
 
 
 def usage() -> str:
@@ -25,63 +25,18 @@ def usage() -> str:
     return path
 
 
-def ensure_yaml_exists(yaml_file_path: str) -> bool:
-    if not os.path.exists(yaml_file_path):
-        logging.error(
-            "No fuzz.yaml file was found\n" "Have you run the auto_detecter command?"
-        )
-        exit(-1)
-    logging.debug(f"YAML file found at {yaml_file_path}")
-
-
-def read_schema_file():
-    yaml_path = os.path.join(os.path.dirname(__file__), "schema.yaml")
-    schema = yamale.make_schema(yaml_path)
-    logging.debug("YAML schema read")
-    return schema
-
-
-def read_yaml_file(path: str):
-    res = yamale.make_data(path)
-    logging.debug(f"YAML file loaded")
-    return res
-
-
-def validate_yaml(yaml_obj, schema):
-    yamale.validate(schema, yaml_obj)
-    logging.debug("YAML file validated")
-
-
-def verify_yaml_file(yaml_file_path: str):
-    schema = read_schema_file()
-    yaml_objs = read_yaml_file(yaml_file_path)
-    validate_yaml(yaml_objs, schema)
-    # yaml_obj is a list of (object, path)
-    # so we return the first item's object
-    return yaml_objs[0][0]
-
-
-def read_and_validate_yaml_file(path: str) -> dict:
-    yaml_file_path = os.path.join(path, "fuzz.yaml")
-    ensure_yaml_exists(yaml_file_path)
-    yaml_obj = verify_yaml_file(yaml_file_path)
-
-    if "TODO" in json.dumps(yaml_obj):
-        logging.warning(
-            "The 'TODO' keyword was found in the yaml file\n"
-            "Did you forget to fill in the blanks?"
-        )
-
-    services: dict = yaml_obj["services"]
-    topics: dict = yaml_obj["topics"]
-    logging.info(
-        f"{len(services.keys())} services detected: {', '.join(services.keys())}"
-    )
-    logging.info(f"{len(topics.keys())} topics detected: {', '.join(topics.keys())}")
-
-    return yaml_obj
-
-
 def main():
     path = usage()
     yaml_obj = read_and_validate_yaml_file(path)
+
+    services: dict = yaml_obj["services"]
+    topics: dict = yaml_obj["topics"]
+
+    # Create fuzzers for services
+    for service_name, service in services.items():
+        logging.info(f"Creating fuzzer for {service_name}")
+        generate_template(
+            source=service["source"],
+            ros_type_str=service["type"],
+            headers_file=service["headers_file"]
+        )
---
+++ ros2_automatic_fuzzer/ros2_fuzzer/yaml_verification.py
@@ -0,0 +1,60 @@
+import logging
+import os
+import json
+import logging
+import yamale
+
+
+def ensure_yaml_exists(yaml_file_path: str) -> bool:
+    if not os.path.exists(yaml_file_path):
+        logging.error(
+            "No fuzz.yaml file was found\n" "Have you run the auto_detecter command?"
+        )
+        exit(-1)
+    logging.debug(f"YAML file found at {yaml_file_path}")
+
+
+def read_schema_file():
+    yaml_path = os.path.join(os.path.dirname(__file__), "schema.yaml")
+    schema = yamale.make_schema(yaml_path)
+    logging.debug("YAML schema read")
+    return schema
+
+
+def read_yaml_file(path: str):
+    res = yamale.make_data(path)
+    logging.debug(f"YAML file loaded")
+    return res
+
+
+def validate_yaml(yaml_obj, schema):
+    yamale.validate(schema, yaml_obj)
+    logging.debug("YAML file validated")
+
+
+def verify_yaml_file(yaml_file_path: str):
+    schema = read_schema_file()
+    yaml_objs = read_yaml_file(yaml_file_path)
+    validate_yaml(yaml_objs, schema)
+    # yaml_obj is a list of (object, path)
+    # so we return the first item's object
+    return yaml_objs[0][0]
+
+
+def read_and_validate_yaml_file(path: str) -> dict:
+    yaml_file_path = os.path.join(path, "fuzz.yaml")
+    ensure_yaml_exists(yaml_file_path)
+    yaml_obj = verify_yaml_file(yaml_file_path)
+
+    if "TODO" in json.dumps(yaml_obj):
+        logging.warning(
+            "The 'TODO' keyword was found in the yaml file\n"
+            "Did you forget to fill in the blanks?"
+        )
+
+    services_keys = yaml_obj["services"].keys()
+    topics_keys = yaml_obj["topics"].keys()
+    logging.info(f"{len(services_keys)} services detected: {', '.join(services_keys)}")
+    logging.info(f"{len(topics_keys)} topics detected: {', '.join(topics_keys)}")
+
+    return yaml_obj
